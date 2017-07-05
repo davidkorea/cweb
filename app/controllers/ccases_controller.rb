d623f1350b72e9379af580930d513aefcb020f3c
OS X <lyfirst@naver.com>
2017-07-05 18:00:16 +0900

    crud-new&create

--- app/controllers/ccases_controller.rb
+++ app/controllers/ccases_controller.rb
@@ -5,7 +5,55 @@ class CcasesController < ApplicationController
     @ccases = Ccase.all
   end
 
-  
+  def show
+    @ccase = Ccase.find(params[:id])
+  end
+
+  def new
+    @ccase = Ccase.new
+  end
+
+
+  def create
+    @ccase = Ccase.new(ccase_params)
+    if @ccase.save
+      redirect_to ccases_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @ccase = Ccase.find(params[:id])
+  end
+
+  def update
+    @ccase = Ccase.find(params[:id])
+    if @ccase.update(ccase_path)
+      redirect_to ccases_path
+      flash[:notice] = "Updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ccase = Ccase.find(params[:id])
+    @ccase.destroy
+    flash[:alert] = "Deleted"
+    redirect_to ccases_path
+  end
+
+
+
+
+  private
+
+  def ccase_params
+    params.require(:ccase).permit(:title, :description, :device, :custname, :custsite, :custphone, :custmail, :note)
+  end
+
 
 
 
