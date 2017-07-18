26e188d760d5ad41f0a5c4529bc99ddb78214079
OS X <lyfirst@naver.com>
2017-07-18 13:24:44 +0900

    admin can change is_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :users
+    root 'ccases#index'
+    resources :ccases
+
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 
   resources :ccases do
     resources :posts
   end
   root 'ccases#index'
 
-  namespace :admin do
-    resources :ccases
-  end
+
 
   namespace :account do
     resources :users
